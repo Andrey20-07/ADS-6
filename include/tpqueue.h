209f9811c3aa9100e945dc6fdf8bae5f14e1ade8
Andrey20-07 <mr.klach999@gmail.com>
2025-04-25 16:47:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,18 +1,19 @@
 // Copyright 2022 NNTU-CS
+#include <list>
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T>
 class TPQueue {
-  private:
+ private:
     std::list<T> lists;
-  public:
+ public:
     void push(const T& element) {
       auto ukaz = lists.begin();
       while (ukaz < lists.end() && ukaz.prior >= element.prior) {
         ukaz++;
       }
-      lists.insert(ukaz,element);
+      lists.insert(ukaz, element);
     }
     T pop() {
       T DelElement = lists.front();
